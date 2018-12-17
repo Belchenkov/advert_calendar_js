180408cbdc277aa82ca1e7156188643bb460e665
Алексей Бельченков <u608110@gmail.com>
2018-12-17 18:06:44 +0400

    Display images in calendar

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const calendarContainer = document.querySelector('.container');
 
 const calendarDays = 24;
 
+const openDoor = (path, event) => {
+    event.target.parentNode.style.backgroundImage = `url(${path})`;
+};
+
 const  createCalendar = () => {
     for (let i = 0; i < calendarDays; i++) {
         const calendarDoor = document.createElement('div');
@@ -11,6 +15,15 @@ const  createCalendar = () => {
         calendarDoor.classList.add('image');
         calendarDoor.style.gridArea = "door" + (i + 1);
         calendarContainer.appendChild(calendarDoor);
+
+        calendarDoorText.classList.add('text');
+        calendarDoorText.innerHTML = i + 1;
+        calendarDoor.appendChild(calendarDoorText);
+
+        let courseNumber = i + 1;
+        let coursePath = `./courses/course-${courseNumber}.jpg`;
+
+        calendarDoorText.addEventListener('click', openDoor.bind(null, coursePath));
     }
 };
 
